c1d89c0dea05b801945e23e95a0e140cbfecbe13
Andrew Norman <anorman@fnal.gov>
2024-03-21 16:58:43 -0500

    Updated get ticket to support osx and linux

--- bin/GetTicket.sh
+++ bin/GetTicket.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
-source ~/.bashrc
+#source ~/.bashrc
 #
 # Set the principal name to use and the keytab
 # that contains the principal
 pname="dune-controlroom-fnal/dune/dune-cr-01@FNAL.GOV"
 keytab="dune-controlroom-fnal.keytab"
+keytabpath="$HOME/.krb5"
 #
 #
 CRPATH="../Base/"
 #
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source ${DIR}/SetupROCOptions.sh
+#source ${DIR}/SetupROCOptions.sh
 
 # Get the ticket using the keytab
-kinit -5 -F -A -k -t $HOME/.krb5/$keytab $pname 2>/dev/null
+
+# This is the Linux version
+#kinit -5 -F -A -k -t $HOME/.krb5/$keytab $pname 2>/dev/null
+
+# This is the OSX version
+if [[ $TERM_PROGRAM == "Apple_Terminal" ]]; then
+    echo "Using Apple Kerberos"
+    kinit -f -A -k -t $keytabpath/$keytab $pname &> /dev/null
+else
+    # This is the Linux version 
+    kinit -5 -F -A -k -t $keytabpath/$keytab $pname 2>/dev/null 
+fi
 
 #
 # Verify that the ticket is in place
-klist
+#klist
 
 # Dipslay a graphic to show key was obtained
 #display $CRPATH/graphics/ticket.jpg &
