72797e9590abcf53400ddf48e945cba4ff6588a2
Andrew Norman <anorman@fnal.gov>
2025-03-06 15:23:21 -0600

    Updated to work with a token

--- bin/list_issues_mu2e.sh
+++ bin/list_issues_mu2e.sh
@@ -1,7 +1,13 @@
 #
-# This script clones all the repositories that are part of the 
+# This script list all the repositories that are part of the 
 # mu2e organization to the local directory structure
 
+#
+# Set a token for the CLI to use
+GH_TOKEN=`cat ~/.credentials/github.token`
+
+# Get a list of repositories and then list
+# the issues for each of them
 gh repo list mu2e --limit 4000 | while read -r repo _; do 
   gh issue list --repo "$repo"
 done     
