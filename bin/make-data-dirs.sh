19e2a3a0fbd559f674b9ccbaea7b8576bd60b95a
Andrew Norman <anorman@fnal.gov>
2026-04-21 14:26:15 -0500

    Script to make standard dirs

---
+++ bin/make-data-dirs.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# Script to make the different data areas for the Mu2eDAQ setup
+#
+# We provide this script so that everything is consistant
+# across nodes in the clusters
+
+import sys
+import os
+import json
+
+# First specify the prefix lists
+prefixList = ["data","data-2","test-data","scratch"]
+runtype = ["test","prod","commissioning","study","other"]
+detectorList = ["calo","trk","crv","stm","extmon","aux","other"]
+
+# Make the primary data areas:
+#
+for thePrefix in prefixList :
+    for theType in runtype :
+        for theDet in detectorList :
+            thePath = "/" + thePrefix + "/" + theType + "/" + theDet "/"
+            try:
+                os.makedirs(thePath, exist_ok=True)
+                print(f"Directory '{thePath}' created successfully.")
+            except Exception as e:
+                print(f"Error creating directory: {e}")
